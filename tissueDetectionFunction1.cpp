503f5ef15e65383364a0d95bcd3524024d41d4f4
MarkSiegelJr <31543236+MarkSiegelJr@users.noreply.github.com>
2018-06-25 13:48:30 -0500

    colorDetection1 implemented as a function

---
+++ tissueDetectionFunction1.cpp
@@ -0,0 +1,60 @@
+/*
+tissueDetectionFunction1.cpp
+Date:06/25/2018
+Author: Mark Siegel
+
+colorDetection1.cpp implemented as a function, more easily transfered to other programs.
+*/
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+bool tissueDetector(Mat bgrImage) {
+	//converting bgr image to hsv image
+	Mat hsvImage;
+	cvtColor(bgrImage, hsvImage, COLOR_BGR2HSV);
+
+	//calculaing image size
+	double imageSize = hsvImage.cols*hsvImage.rows;
+
+	//converting the image into a form where tissue appears white
+	//and everything else appears black.
+	Mat tissue;
+	inRange(hsvImage, Scalar(130, 55, 50), Scalar(179, 255, 255), tissue);
+
+	//Calculating percentage of image is white (is tissue)
+	double tissuePercent = 100.0*((double)countNonZero(tissue)) / imageSize;
+
+	//Background images will yeild 0 percent.
+	if (tissuePercent == 0) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+
+int main(int argc, char** argv)
+{
+	
+	Mat image = imread(argv[1], 1);
+
+	
+	cout << "Tissue present: ";
+	if (tissueDetector(image) == true) {
+		cout << "Yes" << endl;
+	}
+
+	else {
+		cout << "No" << endl;
+	}
+
+    return 0;
+}
+
